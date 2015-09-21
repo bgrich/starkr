9ab6c117eb5d0567a5bca6067d518162975b7156
bgrich <brian.g.richards@gmail.com>
2015-09-21 13:27:50 -0400

    Adjusts spacing for clebsch_gordan2.

--- R/matrix_element_extra_parts.R
+++ R/matrix_element_extra_parts.R
@@ -102,16 +102,16 @@ wigner_3j <- function(j1,j2,j3,m1,m2,m3){
 #'
 #' @examples
 #' clebsch_gordan2(1, 1 / 2, 0, 1 / 2, 3 / 2, 1 / 2)
-clebsch_gordan2 <- function(j1,j2,m1,m2,j,mj){
+clebsch_gordan2 <- function(j1, j2, m1, m2, j, mj){
 
   #If case for the conditions that m1+m2 = mj
-  if((m1+m2)!=mj){
+  if((m1 + m2) != mj){
     CG <- 0
   } else {
     #Calculates the terms that are not in the summation
-    NotSumTerm1 <- sqrt((2*j+1)*factorial(j1+j2-j)*factorial(j1-j2+j)*factorial(-j1+j2+j)/factorial(j1+j2+j+1))
+    NotSumTerm1 <- sqrt((2 * j + 1) * factorial(j1 + j2 - j) * factorial(j1 - j2 + j) * factorial(-j1 + j2 + j) / factorial(j1 + j2 + j + 1))
 
-    NotSumTerm2 <- sqrt(factorial(j1+m1)*factorial(j1-m1)*factorial(j2+m2)*factorial(j2-m2)*factorial(j+mj)*factorial(j-mj))
+    NotSumTerm2 <- sqrt(factorial(j1 + m1) * factorial(j1 - m1) * factorial(j2 + m2) * factorial(j2 - m2) * factorial(j + mj) * factorial(j - mj))
 
     #Settings for the summation. Summation only counts up to s = 4999. If j's and m's are such that one of the terms would be beyond N, then the code will not handle all possible cases.
     N <- 5000
@@ -120,25 +120,25 @@ clebsch_gordan2 <- function(j1,j2,m1,m2,j,mj){
     sum_s <- 0
     for(i in 1:N){
       D1 <- s
-      D2 <- j1+j2-j-s
-      D3 <- j1-m1-s
-      D4 <- j2+m2-s
-      D5 <- j-j2+m1+s
-      D6 <- j-j1-m2+s
+      D2 <- j1 + j2 - j - s
+      D3 <- j1 - m1 - s
+      D4 <- j2 + m2 - s
+      D5 <- j - j2 + m1 + s
+      D6 <- j - j1 - m2 + s
 
       #If any of the denominator terms (D1 to D6) are negative then the term returns zero
-      if((D1<0)|(D2<0)|(D3<0)|(D4<0)|(D5<0)|(D6<0)){
+      if((D1 < 0) | (D2 < 0) | (D3 < 0) | (D4 < 0) | (D5 < 0) | (D6 < 0)){
         sum_s <- 0
       } else{
         #Calculates the terms in the summation at this step of s
-        sum_s <- (-1)^s*(factorial(D1)*factorial(D2)*factorial(D3)*factorial(D4)*factorial(D5)*factorial(D6))^(-1)
+        sum_s <- (-1) ^ s * (factorial(D1) * factorial(D2) * factorial(D3) * factorial(D4) * factorial(D5) * factorial(D6)) ^ (-1)
 
       }
       #Adds current term to the sum and increments s
-    sum <- sum+sum_s
-    s <- s+1
+    sum <- sum + sum_s
+    s <- s + 1
     }
-    CG <- (NotSumTerm1*NotSumTerm2)*sum
+    CG <- (NotSumTerm1 * NotSumTerm2) * sum
   }
 
   CG
