a0a9c906fffa400b1449f12f38e519cdf25aa3cd
bgrich <brian.g.richards@gmail.com>
2015-09-23 10:30:04 -0400

    Add documentation to radial_matrix_element.

--- R/radial_matrix_element.R
+++ R/radial_matrix_element.R
@@ -1,53 +1,70 @@
-##Script for determining the radial matrix element for two arbitrary states in Rubidium-85. 2x faster version. This version also includes the adjusted Quantum Defect.
-
-RadialMatrixElement <- function(k,n1,n2,l1,l2,j1,j2){
+#' Radial Matrix Element.
+#'
+#' \code{radial_matrix_element} calculates the radial matrix element.
+#'
+#' This function calculates the radial matrix element for two arbitrary states
+#' (n1, l1, j1) and (n2, l2, j2). A Numerov algorithm is used to compute the
+#' radial matrix elements as done in Appendix A of Zimmerman et al, PRA, 20, 2251
+#' (1979). The scaling used in this function is \eqn{ksi = sqrt{r},
+#' \eqn{Psi = r^{3/4}R(r)} as done by Bhatti, Cromer, and Cooke, PRA, 24, 161
+#' (1981).
+#'
+#' @param k A numeric. The power of r to be calculated over. To get a dipole
+#' matrix element, k must be equal to 1.
+#' @param n1 A numeric. The principle quantum number of state 1.
+#' @param n2 A numeric. The principle quantum number of state 2.
+#' @param l1 A numeric. The orbital angular momentum number of state 1.
+#' @param l2 A numeric. The orbital angular momentum number of state 2.
+#' @param j1 A numeric. The total angular momentum number of state 1.
+#' @param j2 A numeric. The total angular momentum number of state 2.
+radial_matrix_element <- function(k,n1,n2,l1,l2,j1,j2){
   #Constants and Settings
   #Number of Electrons
   Z <- 1
-  
+
   #Quantum numbers for states 1 and 2. Primary quantum number n, orbital angular momentum l, total angular momentum j
   delta1 <- QuantumDefect(n1,l1,j1)
   delta2 <- QuantumDefect(n2,l2,j2)
   E1 <- -1/(2*(n1-delta1)^2)
   E2 <- -1/(2*(n2-delta2)^2)
-  
+
   #Inner and outer turning points, core radius for both states
   r1_O <- 2*n1*(n1+15)
   r1_I <- (n1^2 - n1*sqrt(n1^2 - l1*(l1+1)))/2
   r2_O <- 2*n2*(n2+15)
   r2_I <- (n2^2 - n2*sqrt(n2^2 - l2*(l2+1)))/2
-  
+
   #Core radius as a function of core polarizability r = (a_c)^(1/3)
   #Core polarizability is a_c = 9.0760 for Rubidium
   core.radius <- (9.0760)^(1/3)
-  
+
   #Determine which outer turning point is the larger to set as starting point
   r_0 <- max(r1_O, r2_O)
-  
-  #Defining scaled x-axis ksi = sqrt(r), step size h, and starting point ksi_0 = sqrt(r_0) 
+
+  #Defining scaled x-axis ksi = sqrt(r), step size h, and starting point ksi_0 = sqrt(r_0)
   ksi_0 <- sqrt(r_0)
   h <- 0.01
   ksi_1 <- ksi_0 - h
   ksi_2 <- ksi_1 - h
-  
+
   #Initial wavefunction guesses
   Psi1_0 <- 10^-15
   Psi1_1 <- 10^-14
-  
+
   Psi2_0 <- 10^-15
   Psi2_1 <- 10^-14
-  
+
   #Defining terms to be used in Numerov Algorithm
   ksi_iminus1 <- ksi_0
   ksi_i <- ksi_1
   ksi_iplus1 <- ksi_2
-  
+
   Psi1_iminus1 <- Psi1_0
   Psi1_i <- Psi1_1
-  
+
   Psi2_iminus1 <- Psi2_0
   Psi2_i <- Psi2_1
-  
+
   #Establishing Numerov integration data frame
   ksi <- numeric()
   Psi1 <- numeric()
@@ -55,7 +72,7 @@ RadialMatrixElement <- function(k,n1,n2,l1,l2,j1,j2){
   N1_i <- numeric()
   N2_i <- numeric()
   Psi12 <- numeric()
-  
+
   if(r1_O < r2_O){
     ksi <- c(ksi, ksi_0, ksi_1)
     Psi1 <- c(Psi1, 0, 0)
@@ -71,7 +88,7 @@ RadialMatrixElement <- function(k,n1,n2,l1,l2,j1,j2){
     N2_i <- c(N2_i, 0, 0)
     Psi12 <- c(Psi12, 0, 0)
   }
-  
+
   #Numerov Algorithm
   #Iterates algorithm until the condition of ksi_(i+1) < sqrt(r_I) or ksi_(i+1) < sqrt(core.radius) is met
   repeat{
@@ -82,12 +99,12 @@ RadialMatrixElement <- function(k,n1,n2,l1,l2,j1,j2){
         g2_iplus1  <- -8*(ksi_iplus1^2*E2 + Z - (l2 + 1/4)*(l2 + 3/4)/(2*ksi_iplus1^2))
         g2_i       <- -8*(ksi_i^2*E2 + Z - (l2 + 1/4)*(l2 + 3/4)/(2*ksi_i^2))
         g2_iminus1 <- -8*(ksi_iminus1^2*E2 + Z - (l2 + 1/4)*(l2 + 3/4)/(2*ksi_iminus1^2))
-        
+
         Psi2_iplus1 <- (Psi2_iminus1*(g2_iminus1 - 12/h^2)+Psi2_i*(10*g2_i+24/h^2))/(12/h^2 - g2_iplus1)
-        
+
         N1_iplus1 <- 0
         N2_iplus1 <- 2*ksi_iplus1^2*Psi2_iplus1^2*h
-        
+
         if(ksi_iplus1<sqrt(max(r1_I,r2_I))|ksi_iplus1<sqrt(core.radius)){
           break
         } else {
@@ -98,17 +115,17 @@ RadialMatrixElement <- function(k,n1,n2,l1,l2,j1,j2){
           N2_i <- c(N2_i, N2_iplus1)
           Psi12 <- c(Psi12, 0)
         }
-        
+
       } else{
         g1_iplus1  <- -8*(ksi_iplus1^2*E1 + Z - (l1 + 1/4)*(l1 + 3/4)/(2*ksi_iplus1^2))
         g1_i       <- -8*(ksi_i^2*E1 + Z - (l1 + 1/4)*(l1 + 3/4)/(2*ksi_i^2))
         g1_iminus1 <- -8*(ksi_iminus1^2*E1 + Z - (l1 + 1/4)*(l1 + 3/4)/(2*ksi_iminus1^2))
-        
+
         Psi1_iplus1 <- (Psi1_iminus1*(g1_iminus1 - 12/h^2)+Psi1_i*(10*g1_i+24/h^2))/(12/h^2 - g1_iplus1)
-        
+
         N1_iplus1 <- 2*ksi_iplus1^2*Psi1_iplus1^2*h
         N2_iplus1 <- 0
-        
+
         if(ksi_iplus1<sqrt(max(r1_I,r2_I))|ksi_iplus1<sqrt(core.radius)){
           break
         } else {
@@ -118,11 +135,11 @@ RadialMatrixElement <- function(k,n1,n2,l1,l2,j1,j2){
           N1_i <- c(N1_i, N1_iplus1)
           N2_i <- c(N2_i, N2_iplus1)
           Psi12 <- c(Psi12, 0)
-          
+
         }
       }
-      
-      
+
+
       if(r1_O<r2_O){
         Psi2_iminus1 <- Psi2_i
         Psi2_i <- Psi2_iplus1
@@ -130,25 +147,25 @@ RadialMatrixElement <- function(k,n1,n2,l1,l2,j1,j2){
         Psi1_iminus1 <- Psi1_i
         Psi1_i <- Psi1_iplus1
       }
-      
+
     } else{
-      
+
       g1_iplus1  <- -8*(ksi_iplus1^2*E1 + Z - (l1 + 1/4)*(l1 + 3/4)/(2*ksi_iplus1^2))
       g1_i       <- -8*(ksi_i^2*E1 + Z - (l1 + 1/4)*(l1 + 3/4)/(2*ksi_i^2))
       g1_iminus1 <- -8*(ksi_iminus1^2*E1 + Z - (l1 + 1/4)*(l1 + 3/4)/(2*ksi_iminus1^2))
       g2_iplus1  <- -8*(ksi_iplus1^2*E2 + Z - (l2 + 1/4)*(l2 + 3/4)/(2*ksi_iplus1^2))
       g2_i       <- -8*(ksi_i^2*E2 + Z - (l2 + 1/4)*(l2 + 3/4)/(2*ksi_i^2))
       g2_iminus1 <- -8*(ksi_iminus1^2*E2 + Z - (l2 + 1/4)*(l2 + 3/4)/(2*ksi_iminus1^2))
-      
+
       Psi1_iplus1 <- (Psi1_iminus1*(g1_iminus1 - 12/h^2)+Psi1_i*(10*g1_i+24/h^2))/(12/h^2 - g1_iplus1)
       Psi2_iplus1 <- (Psi2_iminus1*(g2_iminus1 - 12/h^2)+Psi2_i*(10*g2_i+24/h^2))/(12/h^2 - g2_iplus1)
-      
+
       Psi12_iplus1 <- 2*Psi1_iplus1*Psi2_iplus1*ksi_iplus1^(2+2*k)*h
       N1_iplus1 <- 2*ksi_iplus1^2*Psi1_iplus1^2*h
       N2_iplus1 <- 2*ksi_iplus1^2*Psi2_iplus1^2*h
-      
+
       new.row <- data.frame(ksi = ksi_iplus1, Psi1 = Psi1_iplus1, Psi2 = Psi2_iplus1, N1_i = N1_iplus1, N2_i=N2_iplus1, Psi12 = Psi12_iplus1)
-      
+
       if(ksi_iplus1<sqrt(max(r1_I,r2_I))|ksi_iplus1<sqrt(core.radius)){
         break
       } else {
@@ -164,12 +181,12 @@ RadialMatrixElement <- function(k,n1,n2,l1,l2,j1,j2){
       Psi2_iminus1 <- Psi2_i
       Psi2_i <- Psi2_iplus1
     }
-    
+
     ksi_iminus1 <- ksi_i
     ksi_i <- ksi_iplus1
-    ksi_iplus1 <- ksi_iplus1 - h 
+    ksi_iplus1 <- ksi_iplus1 - h
   }
-  
+
   RadialMatrixElement <- sum(Psi12)/(sqrt(sum(N1_i))*sqrt(sum(N2_i)))
   RadialMatrixElement
 }
