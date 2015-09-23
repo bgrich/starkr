0e9818963cc94c6f13039e30f720bee1a48d7157
bgrich <brian.g.richards@gmail.com>
2015-09-23 12:01:08 -0400

    Re-flow comments.

--- R/stark_matrix_element.R
+++ R/stark_matrix_element.R
@@ -18,14 +18,16 @@
 #' @param mj2 A numeric. The magnetic angular momentum number of state 2.
 stark_matrix_elem <- function(n1, n2, l1, l2, j1, j2, mj1, mj2){
 
-  #Determines if the two mj terms are the same, if they are not then the matrix element is set to zero
+  # Determines if the two mj terms are the same, if they are not then the matrix
+  # element is set to zero
   if(mj1 != mj2){
     StarkElem <- 0
   } else{
-    #Determines if l2 = l1 +/-1. If not, the matrix element is set to zero.
+    # Determines if l2 = l1 +/-1. If not, the matrix element is set to zero.
     if((l2 == (l1 + 1)) | (l2 == (l1 - 1))){
 
-      #Calculates the spherical harmonic matrix element for mj1+1/2. If it is equal to zero, sets that term in the summation equal to zero.
+      # Calculates the spherical harmonic matrix element for mj1+1/2. If it is
+      # equal to zero, sets that term in the summation equal to zero.
       if(sphere_mat_element(l1, l2, mj1 + 1 / 2) == 0){
 
         SumPlus <- 0
@@ -35,7 +37,8 @@ stark_matrix_elem <- function(n1, n2, l1, l2, j1, j2, mj1, mj2){
         SumPlus <- clebsch_gordan(l1, 1/2, mj1 + 1 / 2, -1 / 2, j1, mj1) * clebsch_gordan(l2, 1 / 2, mj1 + 1 / 2, -1 / 2, j2, mj1) * sphere_mat_element(l1, l2, mj1 + 1 / 2)
 
       }
-      #Calculates the spherical harmonic matrix element for mj1-1/2. If it is equal to zero, sets that term in the summation equal to zero.
+      # Calculates the spherical harmonic matrix element for mj1-1/2. If it is
+      # equal to zero, sets that term in the summation equal to zero.
       if(sphere_mat_element(l1, l2, mj1 - 1 / 2) == 0){
 
         SumMinus <- 0
@@ -46,7 +49,7 @@ stark_matrix_elem <- function(n1, n2, l1, l2, j1, j2, mj1, mj2){
 
       }
 
-      #Calculates the stark matrix element.
+      # Calculates the stark matrix element.
       StarkElem <- radial_matrix_element(1, n1, n2, l1, l2, j1, j2) * (SumPlus + SumMinus)
 
     } else{
