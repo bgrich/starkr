cb1b752a881a7dc8726d3adbc03b43ee9d5542df
bgrich <brian.g.richards@gmail.com>
2015-09-23 12:00:05 -0400

    Adjust spacing and fix function names.

--- R/stark_matrix_element.R
+++ R/stark_matrix_element.R
@@ -23,31 +23,31 @@ stark_matrix_elem <- function(n1, n2, l1, l2, j1, j2, mj1, mj2){
     StarkElem <- 0
   } else{
     #Determines if l2 = l1 +/-1. If not, the matrix element is set to zero.
-    if((l2==(l1+1))|(l2==(l1-1))){
+    if((l2 == (l1 + 1)) | (l2 == (l1 - 1))){
 
       #Calculates the spherical harmonic matrix element for mj1+1/2. If it is equal to zero, sets that term in the summation equal to zero.
-      if(SphereMatElement(l1,l2,mj1+1/2)==0){
+      if(sphere_mat_element(l1, l2, mj1 + 1 / 2) == 0){
 
         SumPlus <- 0
 
       } else{
 
-        SumPlus <- Clebsch_Gordan(l1,1/2,mj1+1/2,-1/2,j1,mj1)*Clebsch_Gordan(l2,1/2,mj1+1/2,-1/2,j2,mj1)*SphereMatElement(l1,l2,mj1+1/2)
+        SumPlus <- clebsch_gordan(l1, 1/2, mj1 + 1 / 2, -1 / 2, j1, mj1) * clebsch_gordan(l2, 1 / 2, mj1 + 1 / 2, -1 / 2, j2, mj1) * sphere_mat_element(l1, l2, mj1 + 1 / 2)
 
       }
       #Calculates the spherical harmonic matrix element for mj1-1/2. If it is equal to zero, sets that term in the summation equal to zero.
-      if(SphereMatElement(l1,l2,mj1-1/2)==0){
+      if(sphere_mat_element(l1, l2, mj1 - 1 / 2) == 0){
 
         SumMinus <- 0
 
       } else{
 
-        SumMinus <- Clebsch_Gordan(l1,1/2,mj1-1/2,1/2,j1,mj1)*Clebsch_Gordan(l2,1/2,mj1-1/2,1/2,j2,mj1)*SphereMatElement(l1,l2,mj1-1/2)
+        SumMinus <- clebsch_gordan(l1, 1 / 2, mj1 - 1 / 2, 1 / 2, j1, mj1) * clebsch_gordan(l2, 1 / 2, mj1 - 1 / 2, 1 / 2, j2, mj1) * sphere_mat_element(l1, l2, mj1 - 1 / 2)
 
       }
 
       #Calculates the stark matrix element.
-      StarkElem <- RadialMatrixElement(1,n1,n2,l1,l2,j1,j2)*(SumPlus + SumMinus)
+      StarkElem <- radial_matrix_element(1, n1, n2, l1, l2, j1, j2) * (SumPlus + SumMinus)
 
     } else{
       StarkElem <- 0
