b55c764573527c84f5ce55faeac56401c0e73749
bgrich <brian.g.richards@gmail.com>
2015-09-24 16:36:32 -0400

    Fix a typo reference the wrong function in zero_field_energy_mat.

--- R/stark_map.R
+++ R/stark_map.R
@@ -23,7 +23,7 @@ zero_field_energy_mat <- function(n, l, j){
 
   # For loop puts zero field energies on the diagonal of the matrix
   for(i in 1:length(n)){
-    Energy[i, i] <- -1 / (n[i] - QuantumDefect(n[i], l[i], j[i])) ^ 2 / 2
+    Energy[i, i] <- -1 / (n[i] - quantum_defect(n[i], l[i], j[i])) ^ 2 / 2
   }
 
   Energy
