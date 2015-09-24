bc54e3175b89a67d8c59d00d0f496bdfd45de75c
bgrich <brian.g.richards@gmail.com>
2015-09-24 16:08:22 -0400

    Added resonance finding function and documenation code.

--- R/zero_crossing.R
+++ R/zero_crossing.R
@@ -52,3 +52,26 @@ zero_cross <- function(x, y){
     OutputMatrix
   }
 }
+
+#' Two-body Resonance Calculator.
+#'
+#' \code{resonance} calculates the energy crossover of a pair of atoms in state
+#' A and a pair state of atoms in state B and state C
+#'
+#' This function calculates the energy \eqn{(2*E_A - (E_B + E_C))}. The function
+#' requires a data frame with all of the states and their field values such that
+#' Frame$Field gives the electric field values and Frame$Ecm gives the energy in
+#' inverse centimeters (cm^-1). The second argument, AFrame, is a separate data
+#' frame that already has the frame for state A selected. The other two inputs
+#' should be strings that correspond to a the state column in Frame.
+#'
+#' @param Frame A data frame. A data frame containing all of the in the Stark
+#'   map. Needs columns Field, Ecm, and state.
+#' @param AFrame. A data frame. Needs columns Field and Ecm.
+#' @param StateB. A character vector identifying state B.
+#' @param StateC. A character vector identifying state C.
+resonance <- function(Frame, AFrame, StateB, StateC){
+
+  zero_cross(AFrame$Field, (2*AFrame$Ecm - ((Frame%>%filter(state %in% StateB)%>%select(Field, Ecm))$Ecm + (Frame%>%filter(state%in%StateC)%>%select(Field,Ecm))$Ecm)))
+
+}
