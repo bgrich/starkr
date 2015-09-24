a261cdf146659fc4d831853e5a8b092f8e19af92
bgrich <brian.g.richards@gmail.com>
2015-09-24 16:59:14 -0400

    Add function zero_field_energy_df to create a data frame with the zero field energies.

--- R/stark_map.R
+++ R/stark_map.R
@@ -30,3 +30,21 @@ zero_field_energy_mat <- function(n, l, j){
 
 }
 
+
+zero_field_energy_df <- function(n, l, j, mj){
+
+  # Initializes a data frame to hold the zero field energy and information
+  # about the states
+  Energy_df <- data.frame(E0 = numeric(), n = numeric(), l = numeric(), j = numeric(), mj = numeric(), state = character())
+
+  # For loop puts all of the zero energies and states into the data frame
+for(i in 1:length(n)){
+  new_row <- data.frame(E0 = -1 / (n[i] - quantum_defect(n[i], l[i], j[i])) ^ 2 / 2, n = n[i], l = l[i], j = j[i], mj = mj, state = paste(n[i],l[i], j[i], mj, sep = ','))
+
+  Energy_df <- rbind(Energy_df, new_row)
+}
+#Turns the data frame in to a dplyr table.
+Energy_df <- tbl_df(Energy_df)
+
+Energy_df
+}
