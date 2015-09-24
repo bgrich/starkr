cda009ce7db8705c27a5f6354f659507f85bbeaf
bgrich <brian.g.richards@gmail.com>
2015-09-24 16:36:01 -0400

    Add documentation code to zero_field_energy_mat.

--- R/stark_map.R
+++ R/stark_map.R
@@ -1,3 +1,22 @@
+#' Construct Zero Field Energy Matrix.
+#'
+#' \code{zero_field_energy_mat} constructs a matrix with the zero field
+#' energies on the diagonal.
+#'
+#' This function takes vectors for n, l, and j and returns a matrix with the
+#' energies for those n, l, and j states on the diagonal. To create the
+#' vectors for n, l, and j, \link{\code{state_list}} should be used. This will
+#' create a matrix with all the combinations of n, l, and j needed to create
+#' the Stark matrix. The energy is determined by:
+#' \deqn{E = -1 / (2* (n - \delta(n, l, j))^2)}
+#' where \eqn{\delta(n, l, j)} is determined by \link{\code{quantum_defect}}
+#'
+#' @param n A numeric. A vector that contains a series of principle quantum
+#' numbers.
+#' @param l A numeric. A vector that contains a series of orbital angular
+#' momentum quantum numbers.
+#' @param j A numeric. A vector that contains a series of total angular
+#' momentum quantum numbers.
 zero_field_energy_mat <- function(n, l, j){
   # Initializes a matrix to hold the zero field energy
   Energy <- matrix(0, nrow = length(n), ncol = length(n))
@@ -10,3 +29,4 @@ zero_field_energy_mat <- function(n, l, j){
   Energy
 
 }
+
