c8aa200751302a2f2a41d39a4281c874e4ad3956
bgrich <brian.g.richards@gmail.com>
2015-09-24 14:17:31 -0400

    Add a new version of zero_cross to try and speed it up.

--- R/zero_crossing.R
+++ R/zero_crossing.R
@@ -1,10 +1,10 @@
 #Determines the location of zero crossings for two arbitrary vectors. The vector y is the vector whose zero crossing are being measured. The vector x is an associated position vector for y.
 
-ZeroCross <- function(x, y){
+zero_cross1 <- function(x, y){
   #Initiates variables to hold the index and x position for the zero crossing.
   ZeroPos <- numeric()
   ZeroX <- numeric()
-  
+
   for(i in 1:(length(y)-1)){
     #If the value of y[i] is zero then it is kept
     if(sign(y[i]) == 0){
@@ -26,7 +26,36 @@ ZeroCross <- function(x, y){
   #If there are no zero crossings a message is printed. Otherwise, a matrix is sent out with the index and x position of each zero crossing.
   if(length(ZeroPos) == 0){
     print("This vector contains no zero crossings.")
-  } else { 
+  } else {
+    OutputMatrix <- cbind(ZeroPos, ZeroX)
+    colnames(OutputMatrix) <- c("Index", "X.position")
+    OutputMatrix
+  }
+}
+
+
+
+zero_cross2 <- function(x, y){
+  #Initiates variables to hold the index and x position for the zero crossing.
+  ZeroPos <- numeric()
+  ZeroX <- numeric()
+
+  vector_sign <- sign(y)
+
+  zero_pos <- which(vector_sign == 0)
+
+  difference_vector <- diff(vector_sign)
+
+  positive_cross <- which(difference_vector == 2)
+  negative_cross <- which(difference_vector == -2)
+
+  ZeroPos <- c(zero_pos, positive_cross, negative_cross)
+  ZeroX <- c(x[zero_pos], x[positive_cross], x[negative_cross])
+
+  #If there are no zero crossings a message is printed. Otherwise, a matrix is sent out with the index and x position of each zero crossing.
+  if(length(ZeroPos) == 0){
+    print("This vector contains no zero crossings.")
+  } else {
     OutputMatrix <- cbind(ZeroPos, ZeroX)
     colnames(OutputMatrix) <- c("Index", "X.position")
     OutputMatrix
